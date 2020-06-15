3d37a355cb61bbec0c604cc9a56341c4489738bb
sharifhimu <sharifsorotcu@gmail.com>
2020-06-15 15:38:58 +0600

    Updates

---
+++ precache-manifest.9cd6950c38f8fcab1dfb73de619b8334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8cd02182b6216afdd600a060baf7b4",
+    "url": "/tenpixel/index.html"
+  },
+  {
+    "revision": "8275a294107304acdcb7",
+    "url": "/tenpixel/static/css/main.448c3961.chunk.css"
+  },
+  {
+    "revision": "e9928fb2802993f6200f",
+    "url": "/tenpixel/static/js/2.efbc3024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tenpixel/static/js/2.efbc3024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8275a294107304acdcb7",
+    "url": "/tenpixel/static/js/main.c5f94fb4.chunk.js"
+  },
+  {
+    "revision": "16e79cae9bf249105b63",
+    "url": "/tenpixel/static/js/runtime-main.5ee83dc0.js"
+  }
+]);
